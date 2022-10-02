50eca8e7117e545bb84d807360d4accfa872c764
mudjaycker <tombutoyi@gmail.com>
2022-10-02 10:37:27 +0200

    setup model to manage db functionnalities

---
+++ db.py
@@ -0,0 +1,43 @@
+import sqlite3 as sql
+
+class DB:
+    def __init__(self):
+        self.con = sql.connect("todo.db")
+        self.cursor = self.con.cursor()
+        self.create_task_table()
+    
+    def create_task_table(self, task, due_date=None):
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS tasks(id integer PRIMARY KEY AUTOINCREMENT, task varchar(50) NOT NULL, due_date varchar(50), completed BOOLEAN NOT NULL CHECK (completed IN (0, 1)))")
+        self.con.commit()
+        created_task = self.cursor.execute("SELECT id, task, due_date FROM tasks WHERE task = ? and completed = 0", (task,)).fetchall()
+        
+        return created_task[-1]
+
+
+    def get_tasks(self):
+        uncomplete_tasks = self.cursor.execute("SELECT id, task, due_date FROM tasks WHERE completed = 0").fetchall()
+        completed_tasks = self.cursor.execute("SELECT id, task, due_date FROM tasks WHERE completed = 1").fetchall()
+
+        return completed_tasks, uncomplete_tasks
+
+    
+
+    def mark_task_as_complete(self, taskid):
+        self.cursor.execute("UPDATE tasks SET completed=1 WHERE id=?", (taskid,))
+        self.con.commit()
+
+    def mark_task_as_incomplete(self, taskid):
+        """Mark task as uncomplete"""
+        self.cursor.execute("UPDATE tasks SET completed=0 WHERE id=?", (taskid,))
+        self.con.commit()
+
+        task_text = self.cursor.execute("SELECT task FROM tasks WHERE id=?", (taskid,)).fetchall()
+        
+        return task_text[0][0]
+
+    def delete_task(self, taskid):
+        self.cursor.execute("DELETE FROM tasks WHERE id=?", (taskid,))
+        self.con.commit()
+
+    def close_db_connection(self):
+        self.con.close()
